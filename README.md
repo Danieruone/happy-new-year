0584dc16246736f283f630c3d8b299cc17349fc9
Daniel <danieruone@gmail.com>
2020-12-31 21:01:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Happy 2021
+
+Hope you learn a lot this year!
+
+![Page preview](images/capture.PNG)
---
+++ script.js
@@ -0,0 +1,3 @@
+var confettiSettings = { target: 'canvas' };
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
